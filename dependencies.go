963e07345d28b220ca42afd7fcb86a5de6e332ec
Steve Kuznetsov <skuznets@redhat.com>
2022-10-05 10:08:59 -0600

    dependencies: snip a dependency we don't need
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- dependencies.go
+++ dependencies.go
@@ -21,7 +21,6 @@ import (
 	_ "github.com/kcp-dev/logicalcluster/v2"
 
 	_ "k8s.io/api/core/v1"
-	_ "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	_ "k8s.io/apimachinery/pkg/labels"
 	_ "k8s.io/client-go/listers/core/v1"
 )
